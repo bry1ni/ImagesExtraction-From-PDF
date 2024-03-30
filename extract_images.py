090abc88805f76a5eb926932f08d473ec49b68fc
Ryan <106076362+bry1ni@users.noreply.github.com>
2024-03-30 23:48:34 +0100

    Update extract_images.py

--- extract_images.py
+++ extract_images.py
@@ -5,56 +5,23 @@ import fitz  # PyMuPDF
 
 
 def extract_images_from_pdf(pdf_path, output_folder, min_width=0, min_height=0, min_area=0, output_format="png"):
-    collage = False
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
     pdf_file = fitz.open(pdf_path)
     image_paths = []
 
-    # Iterate over PDF pages
+    # PDF pages
     for page_index in range(len(pdf_file)):
-        # Get the page itself
         page = pdf_file[page_index]
-        # Get image list
-        image_list = page.get_images(full=True)
-        # Print the number of images found on this page
+        image_list = page.get_images(full=True) # get images present in the page
+        
         if image_list:
             print(f"[+] Found a total of {len(image_list)} images in page {page_index}")
-            if len(image_list) > 10:
-                collage = True
         else:
             print(f"[!] No images found on page {page_index}")
-        # Iterate over the images on the page
+        # Images 
         for image_index, img in enumerate(image_list, start=1):
-            if collage:
-                # make a collage of all images
-                images = []
-                for img in image_list:
-                    xref = img[0]
-                    base_image = pdf_file.extract_image(xref)
-                    image_bytes = base_image["image"]
-                    image = Image.open(io.BytesIO(image_bytes))
-                    images.append(image)
-                widths, heights = zip(*(i.size for i in images))
-                total_width = sum(widths)
-                max_height = max(heights)
-                collage = Image.new('RGB', (total_width, max_height))
-                x_offset = 0
-                y_offset = 0
-                for i, img in enumerate(images):
-                    collage.paste(img, (x_offset, y_offset))
-                    x_offset += img.width
-                    if (i + 1) % 5 == 0:  # Start a new row after every 5 images
-                        y_offset += img.height
-                        x_offset = 0
-                collage_path = os.path.join(output_folder, f"collage{page_index + 1}.{output_format}")
-                collage.save(open(collage_path, "wb"), format=output_format.upper())
-                image_paths.append(collage_path)
-                print(f"[+] Collage saved from page {page_index}")
-                collage = False
-
-            else:
                 # Get the XREF of the image
                 xref = img[0]
                 # Extract the image bytes
@@ -79,7 +46,7 @@ def extract_images_from_pdf(pdf_path, output_folder, min_width=0, min_height=0, 
 
 
 pdf_path = "/Users/rayanpicso/Desktop/Humanaize/DATA_retrieval/Data/manual-1st-doc.pdf"
-output_folder = "manual1-pieces-solvedTRY2"  # Output folder to save the images
-min_area = 10000  # Minimum area (width * height) for images to be saved
+output_folder = "manual1-pieces-solvedTRY2"  # output folder
+min_area = 10000  # min resolution
 
 extract_images_from_pdf(pdf_path, output_folder, min_area=min_area)
